bef171dc47b352b162e843c4eb193c04a7559a53
SACHIN SAINI <sachinsaini.ss.ss87@gmail.com>
2022-10-09 19:48:14 +0530

    Added Text-Formatter version 1

---
+++ Text-formatter/Version-1/readme.md
@@ -0,0 +1,5 @@
+It is a text formatter application. It is having follwing functionalities:
+1. Text can be converted into Uppercase.
+2. Text can be converted into Lowercase.
+3. Capitalize every sentence's first letter.
+4. Capitalize every word's first letter.
---
+++ Text-formatter/Version-1/script.js
@@ -0,0 +1,38 @@
+let txt = document.getElementById("inputBox");
+document.getElementsByName('format-option').forEach(function(e,index) {
+    e.addEventListener("click", ()=> {
+        formatText(index);
+    });
+});
+
+function formatText(index){
+    switch(index){
+        case 0: txt.value = txt.value.toUpperCase(); break;        
+        case 1: txt.value = txt.value.toLowerCase(); break;        
+        case 2: txt.value = capitalizeSentence(txt.value); break;  
+        case 3: txt.value = capitalizeWord(txt.value); break;        
+    }
+}
+
+function capitalizeWord(str){    
+    let splitSentence = str.split(" ");    
+    console.log(splitSentence)
+    for (let i = 0; i < splitSentence.length; i++) {        
+        splitSentence[i] = splitSentence[i][0].toUpperCase()+splitSentence[i].slice(1);
+    }    
+    console.log(splitSentence)
+    splitSentence=splitSentence.join(" ");    
+    // console.log(splitSentence)
+    return splitSentence;
+}
+function capitalizeSentence(str){    
+    let splitSentence = str.split(". ");
+    // console.log(splitSentence)
+    for (let i = 0; i < splitSentence.length; i++) {                
+        splitSentence[i] = splitSentence[i][0].toUpperCase()+splitSentence[i].slice(1);
+    }
+    // console.log(splitSentence)
+    splitSentence=splitSentence.join(". ");
+    // console.log(splitSentence)
+    return splitSentence;
+}
